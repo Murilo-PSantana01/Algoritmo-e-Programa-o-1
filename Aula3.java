93e4d6a1d6fb08573a79741daa9783dd5249816a
Murilo-PSantana01 <murilopsantana01@gmail.com>
2026-09-02 09:13:15 -0300

    Add files via upload

---
+++ Aula3.java
@@ -0,0 +1,11 @@
+public class Aula3{
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo");
+        System.out.println("Minha idade é: " + 19 + " anos.");
+        System.out.println("O resuldado é: "+(3+5<=22));
+    
+
+    }
+}
