5f4368451cf4cee914cfb266dd1b9e20732a0f84
Murilo-PSantana01 <murilopsantana01@gmail.com>
2026-09-02 10:45:11 -0300

    Add files via upload

---
+++ Exercio2.java
@@ -0,0 +1,11 @@
+public class Exercio2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("19");
+        System.out.println("1.80");
+        System.out.println("Presente");
+
+    }
+    
+}
